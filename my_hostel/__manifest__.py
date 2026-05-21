18c3bbe895f444c1b9dc63b8c04c86fd1ebb6677
alihassan770 <mehar4276@gmail.com>
2026-05-21 11:54:50 +0500

    {RENAME]manifest

--- my_hostel/__manifest__.py
+++ my_hostel/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Hostel Management",
     'summary': "Manage Hostel easily",
     'description': "Efficiently manage the entire residential facility in the school.", # Supports reStructuredText(RST) format (description is Deprecated),
-    'author': "Your name",
+    'author': "Javeria",
     'category': 'Uncategorized',
     'license': 'LGPL-3',
     'version': '18.0.1.0.0',
